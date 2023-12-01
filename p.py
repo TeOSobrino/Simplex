3a894f794198f9bcbf61739249b90e1a92be3934
Felipe <famdestaole@gmail.com>
2023-11-30 22:29:11 -0300

    mudanca para forma padrao

--- p.py
+++ p.py
@@ -13,24 +13,48 @@ def print_arr(size:int, arr):
 def main():
 
     #devolve uma tupla https://pypi.org/project/pysmps/ da p achar aqui  
-    b = smps.load_mps("/home/teo/Documents/4s/trablin/orig/t1.mps")
-    # b = smps.load_mps("C:\\Users\\famde\\OneDrive\\Documentos\\Programacao\\t1.mps")
+    problema = smps.load_mps("/home/teo/Documents/4s/trablin/orig/t1.mps")
+    # problema = smps.load_mps("C:\\Users\\famde\\OneDrive\\Documentos\\Programacao\\t1.mps")
    
-    tipos_restricao = b[5] # tipo das restrições (eq, leq, geq, lt, gt)
-    c = np.array(b[6]) # função de custo (c)
+    tipos_restricao = problema[5] # tipo das restrições (eq, leq, geq, lt, gt)
+    c = np.array(problema[6]) # função de custo (c)
     
-    A = b[7] # matriz de custo (A)
-    np.array(A)
-    r_n = b[8] # chave de acesso para as restrições ao lado direito
-    r = b[9] # valor das restrições (b)
-    v_n = b[10] # chave para as restrições sobre as variáveis
-    v_r = b[11] # chave para os vetores de acesso das variáveis
+    A = np.array(problema[7]) # matriz de custo (A)
+    r_n = problema[8] # chave de acesso para as restrições ao lado direito
+    r = problema[9] # valor das restrições (b)
+    b = np.array(r[r_n[0]])
+    v_n = problema[10] # chave para as restrições sobre as variáveis
+    v_r = problema[11] # chave para os vetores de acesso das variáveis
     v_acc = v_r[v_n[0]] # dict com as restrições todas
-    v_lo = v_acc['LO'] #restrições de <= para as variávies
-    v_hi = v_acc['UP'] #restrições de >= para as variávies
+    v_lo = np.array(v_acc['LO']) #restrições de <= para as variávies
+    v_hi = np.array(v_acc['UP']) #restrições de >= para as variávies
 
     n_var = A.shape[1]
     n_res = A.shape[0]
+    
+    # Trocando as restriçoes das variaveis para >= 0
+    for index,(restricao_lo,restricao_hi) in enumerate(zip(v_lo,v_hi)):
+        # se for menor que um finito, adicionar restricao 
+        if restricao_lo != np.inf and restricao_lo > 0:
+            # adiciona linha na matriz A
+            n_res += 1
+            linha = np.zeros(n_var)
+            linha[index] = 1
+            A = np.insert(A, A.shape[0], linha, axis=0)
+            # adiciona um valor no final do vetor b
+            b = np.insert(b, b.shape[0], restricao_lo)
+            tipos_restricao.append('G')
+        
+        if restricao_hi != np.inf and restricao_hi > 0:
+            # adiciona linha na matriz A
+            n_res += 1
+            linha = np.zeros(n_var)
+            linha[index] = 1
+            A = np.insert(A, A.shape[0], linha, axis=0)
+            # adiciona um valor no final do vetor b
+            b = np.insert(b, b.shape[0], restricao_hi)
+            tipos_restricao.append('L')
+            
 
     # Mudando as restricoes para "="
     for indicie_restricao, restricao in enumerate(tipos_restricao):
@@ -49,9 +73,10 @@ def main():
         if restricao == 'G':
             A[indicie_restricao] *= -1
         
-            
-        
+    
 
+        
+        
     # pprint(b)
     print(f"{n_var} {n_res}")
     
@@ -61,7 +86,7 @@ def main():
 
     # Salvando o DataFrame em um arquivo Excel
     df.to_excel("matriz_excel.xlsx", index=False)
-    print(r[r_n[0]])
+    print(b)
     print(v_hi)
     print(v_lo)
 
